c0184d22887e6182775e0c6cac61d49978285743
Andrew-P-Albanese <astr0nautj0nesapa@gmail.com>
2020-10-01 17:50:40 -0400

    added phone and email vaildation

--- DevfolioProject/Scripts/contact-me-validation.js
+++ DevfolioProject/Scripts/contact-me-validation.js
@@ -1,16 +1,22 @@
-    function validateForm(){
+function validateEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+function validateForm() {
 	var name = document.forms["contactForm"]["name"].value;
 	var email = document.forms["contactForm"]["email"].value;
 	var phone = document.forms["contactForm"]["phone"].value;
-	var message = document.forms["contactForm"]["message"].value;
+    var message = document.forms["contactForm"]["message"].value;
+    var phonenum = /^\d{10}$/;
 
 	if (name.length < 1) {
         document.getElementById('error-name').innerHTML = " Please Enter A Valid Name *".fontcolor("red");
-        }
-    if (email.length < 1 || email) {
+    }
+    if (!validateEmail(email)) {
         document.getElementById('error-email').innerHTML = " Please Enter A Valid Email *".fontcolor("red");
     }
-    if (phone.length < 1) {
+    if (!phone.value.match(phonenum)) {
         document.getElementById('error-phone').innerHTML = " Please Enter A Valid Phone *".fontcolor("red");
     }
     if (message.length < 1) {
