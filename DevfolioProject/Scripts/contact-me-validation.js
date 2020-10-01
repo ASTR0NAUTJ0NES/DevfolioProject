7f991f01eafaf385d6b6767347197fcd44f533e7
Andrew-P-Albanese <astr0nautj0nesapa@gmail.com>
2020-10-01 16:58:53 -0400

    added validation

--- DevfolioProject/Scripts/contact-me-validation.js
+++ DevfolioProject/Scripts/contact-me-validation.js
@@ -5,16 +5,16 @@
 	var message = document.forms["contactForm"]["message"].value;
 
 	if (name.length<1) {
-        document.getElementById('error-name').innerHTML = " Please Enter Your Name *"
-    }
-    if (email.length<1) {
-        document.getElementById('error-email').innerHTML = " Please Enter Your Email *";
+        document.getElementById('error-name').innerHTML = " Please Enter A Valid Name *"
+        }
+    if (email.length < 1) {
+        document.getElementById('error-email').innerHTML = " Please Enter A Valid Email *";
     }
     if (phone.length<1) {
-        document.getElementById('error-phone').innerHTML = " Please Enter Your Phone *";
+        document.getElementById('error-phone').innerHTML = " Please Enter A Valid Phone *";
     }
     if (message.length<1) {
-        document.getElementById('error-message').innerHTML = " Please Enter Your Message *";
+        document.getElementById('error-message').innerHTML = " Please Enter Your Message*";
     }
     if(name.length<1 || email.length<1 || phone.length<1 || message.length<1){
        	return false;
