7e6ca2a5b8817c45dded66a55b213798e8327fa9
Andrew-P-Albanese <astr0nautj0nesapa@gmail.com>
2020-10-01 17:15:14 -0400

    added form validation

--- DevfolioProject/Scripts/contact-me-validation.js
+++ DevfolioProject/Scripts/contact-me-validation.js
@@ -4,17 +4,17 @@
 	var phone = document.forms["contactForm"]["phone"].value;
 	var message = document.forms["contactForm"]["message"].value;
 
-	if (name.length<1) {
-        document.getElementById('error-name').innerHTML = " Please Enter A Valid Name *"
+	if (name.length < 1) {
+        document.getElementById('error-name').innerHTML = " Please Enter A Valid Name *".fontcolor("red");
         }
     if (email.length < 1) {
-        document.getElementById('error-email').innerHTML = " Please Enter A Valid Email *";
+        document.getElementById('error-email').innerHTML = " Please Enter A Valid Email *".fontcolor("red");
     }
-    if (phone.length<1) {
-        document.getElementById('error-phone').innerHTML = " Please Enter A Valid Phone *";
+    if (phone.length < 1) {
+        document.getElementById('error-phone').innerHTML = " Please Enter A Valid Phone *".fontcolor("red");
     }
-    if (message.length<1) {
-        document.getElementById('error-message').innerHTML = " Please Enter Your Message*";
+    if (message.length < 1) {
+        document.getElementById('error-message').innerHTML = " Please Enter Your Message *".fontcolor("red");
     }
     if(name.length<1 || email.length<1 || phone.length<1 || message.length<1){
        	return false;
