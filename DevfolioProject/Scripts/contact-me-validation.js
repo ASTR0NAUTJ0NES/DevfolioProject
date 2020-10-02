aabb29436508a0fa3b4e046c523fe7f9bbcc576f
Andrew-P-Albanese <astr0nautj0nesapa@gmail.com>
2020-10-02 10:54:18 -0400

    added logic for validator

--- DevfolioProject/Scripts/contact-me-validation.js
+++ DevfolioProject/Scripts/contact-me-validation.js
@@ -22,24 +22,24 @@ function validateForm() {
 	var phone = document.forms["contactForm"]["phone"].value;
     var message = document.forms["contactForm"]["message"].value;
     
-
-	if (!validateName(name)) {
-        document.getElementById('error-name').innerHTML = " Please Enter A Valid Name *".fontcolor("red");
-    }
-    if (!validateEmail(email)) {
-        document.getElementById('error-email').innerHTML = " Please Enter A Valid Email *".fontcolor("red");
-    }
-    if (!validatePhone(phone)) {
-        document.getElementById('error-phone').innerHTML = " Please Enter A Valid Phone *".fontcolor("red");
-    }
-    if (!validateMessage(message)) {
-        document.getElementById('error-message').innerHTML = " Please Enter Your Message *".fontcolor("red");
-    }
-    if(name.length<1 || email.length<1 || phone.length<1 || message.length<1){
-       	return false;
-    }
     if (validateName(name) && validateEmail(email) && validatePhone(phone) && validateMessage(messages)) {
         var elementVar = document.getElementById("contactForm");
         elementVar.setAttribute("data-netlify", "true");
+    } else {
+        if (!validateName(name)) {
+            document.getElementById('error-name').innerHTML = " Please Enter A Valid Name *".fontcolor("red");
+        }
+        if (!validateEmail(email)) {
+            document.getElementById('error-email').innerHTML = " Please Enter A Valid Email *".fontcolor("red");
+        }
+        if (!validatePhone(phone)) {
+            document.getElementById('error-phone').innerHTML = " Please Enter A Valid Phone *".fontcolor("red");
+        }
+        if (!validateMessage(message)) {
+            document.getElementById('error-message').innerHTML = " Please Enter Your Message *".fontcolor("red");
+        }
+        if (name.length < 1 || email.length < 1 || phone.length < 1 || message.length < 1) {
+            return false;
+        }
     }
 }
