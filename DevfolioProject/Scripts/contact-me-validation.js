de994648b636b16787341db2ae1fd19d43ba281c
Andrew-P-Albanese <astr0nautj0nesapa@gmail.com>
2020-10-02 10:49:34 -0400

    added working validation

--- DevfolioProject/Scripts/contact-me-validation.js
+++ DevfolioProject/Scripts/contact-me-validation.js
@@ -1,6 +1,19 @@
+function validateName(name) {
+    return name.length > 1;
+}
+
 function validateEmail(email) {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegEx.test(String(email).toLowerCase());
+}
+
+function validatePhone(phone) {
+    var phonenumRegEx = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+    return phonenumRegEx.test(String(phone));
+}
+
+function validateMessage(message) {
+    return message.length > 1;
 }
 
 function validateForm() {
@@ -8,21 +21,25 @@ function validateForm() {
 	var email = document.forms["contactForm"]["email"].value;
 	var phone = document.forms["contactForm"]["phone"].value;
     var message = document.forms["contactForm"]["message"].value;
-    var phonenum = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+    
 
-	if (name.length < 1) {
+	if (!validateName(name)) {
         document.getElementById('error-name').innerHTML = " Please Enter A Valid Name *".fontcolor("red");
     }
     if (!validateEmail(email)) {
         document.getElementById('error-email').innerHTML = " Please Enter A Valid Email *".fontcolor("red");
     }
-    if (!phone.value.match(phonenum)) {
+    if (!validatePhone(phone)) {
         document.getElementById('error-phone').innerHTML = " Please Enter A Valid Phone *".fontcolor("red");
     }
-    if (message.length < 1) {
+    if (!validateMessage(message)) {
         document.getElementById('error-message').innerHTML = " Please Enter Your Message *".fontcolor("red");
     }
     if(name.length<1 || email.length<1 || phone.length<1 || message.length<1){
        	return false;
+    }
+    if (validateName(name) && validateEmail(email) && validatePhone(phone) && validateMessage(messages)) {
+        var elementVar = document.getElementById("contactForm");
+        elementVar.setAttribute("data-netlify", "true");
     }
 }
